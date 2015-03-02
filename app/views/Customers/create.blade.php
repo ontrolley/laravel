4904d23958e356746e52c4eca260e0b8add7d9d6
Andrejs Griscuks <andrejsgriscuks@gmail.com>
2015-03-02 19:30:51 +0200

    Change routes

--- app/views/Customers/create.blade.php
+++ app/views/Customers/create.blade.php
@@ -4,7 +4,7 @@
 	<div class="col-md-3">
 	</div>
 	<div class="col-md-6">			
-		{{ Form::model(new Customer, ['route' => ['customers/store'], 'files' => [true]]) }}
+		{{ Form::model(new Customer, ['route' => ['Customers.store'], 'files' => [true]]) }}
 			@include('Customers/partials/_form', ['submit_text' => 'Add info'])
     {{ Form::close() }}
 	</div>
