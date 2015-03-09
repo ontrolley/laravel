ee8c8942f6b96cff62c43d0b503d912ff6de058d
Andrejs Griscuks <andrejsgriscuks@gmail.com>
2015-03-09 20:52:00 +0200

    Add migration

---
+++ app/database/migrations/2015_03_09_184628_add_some_fields_to_customers.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSomeFieldsToCustomers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::table('customers', function(Blueprint $table)
+    {
+        $table->string('password', 64);
+        $table->string('lastname', 100);
+        $table->strint('remember_token', 100);
+    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		$table->dropColumn('password', 64);
+		$table->dropColumn('lastname', 100);
+		$table->dropColumn('remember_token', 100);
+	}
+
+}
